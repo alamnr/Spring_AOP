2eb50ebdac0afe1941ecf30f779fb0ec053ed47f
alamnr <alamnr@gmail.com>
2018-04-14 15:59:31 +0600

    Commit for Aop tutorial 28

--- src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -2,13 +2,33 @@ package org.koushik.javabrains.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String getName())")
+	//@Before("execution(public String org.koushik.javabrains.model.Circle.getName())")
+	//@Before("execution(public String getName())")
+	//@Before("execution(public String get*())")
+	//@Before("execution(public * get*())")
+	//@Before("execution( * get*())")
+	//@Before("execution( * get*(*))")
+	//@Before("execution( * get*(..))")
+	//@Before("execution( * org.koushik.javabrains.model.*.get*(..))")
+	@Before("allGetters()")
 	public void LoggingAdvice(){
 		System.out.println("Advice run. Get Method Called");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice(){
+		System.out.println("Second Advice executed.");
+	}
+	
+	@Pointcut("execution( * get*())")
+	public void allGetters(){
+		
+	}
+	
 
 }
