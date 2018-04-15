e1ad2b678db8ae3610bbcf564480682b8e649e1c
alamnr <alamnr@gmail.com>
2018-04-15 16:38:56 +0600

    Commit for Tutorial 34

--- src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -11,43 +11,11 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.koushik.javabrains.model.Circle;
 
-@Aspect
+//@Aspect
 public class LoggingAspect {
 	
 	
-	@Before("allCircleMethods()")
-	public void LoggingAdvice(JoinPoint joinPoint){
-		//System.out.println("Advice run. Get Method Called");
-		//System.out.println(joinPoint.toString());
-		/*System.out.println(joinPoint.getTarget());
-		Circle  circle = (Circle)joinPoint.getTarget();*/
-	}
-	
-	//@Before("args(String)")
-	//@Before("args(name)") // method having single param
-	//@After("args(name)")
-	/*@AfterReturning("args(name)") // run only when method return without any exception
-	public void stringArgumentMethods(String name){
-		System.out.println("A method that takes string arguments has been calle. The value is- "+name);
-	}*/
-	
-	@AfterReturning(pointcut="args(name)",returning="returnString") // run only when method return without any exception
-	public void stringArgumentMethods(String name,Object returnString){
-		System.out.println("A method that takes string arguments has been called. The value is- "+name +" and the output value is : "+returnString);
-	}
-	
-	/*@AfterThrowing("args(name)")
-	public void exceptionAdvice(String name){
-		System.out.println("An exception has been thrown.");
-	}*/
-	
-	@AfterThrowing(pointcut="args(name)",throwing="ex")
-	public void exceptionAdvice(String name, RuntimeException ex){
-		System.out.println("An exception has been thrown.  " +  ex);
-	}
-	
 	//@Around("allGetters()")
-	@Around("@annotation(org.koushik.javabrains.aspect.Loggable)")
 	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint){
 		
 		Object returnValue=null;
@@ -67,17 +35,11 @@ public class LoggingAspect {
 		return returnValue;
 	}
 	
-	@Pointcut("execution( * get*())")
+	/*@Pointcut("execution( * get*())")
 	public void allGetters(){
 		
-	}
+	}*/
 	
-	//@Pointcut("execution(* org.koushik.javabrains.service.*.*(..))")
-	//@Pointcut("execution(* org.koushik.javabrains.service.*Service.*(..))")
-	
-	
-	@Pointcut("within(org.koushik.javabrains.model.Circle)")
-	public void allCircleMethods(){}
 	
 	
 }
